5c2454f8026006b1f183c2d9d761c45e0df877a6
Charu Saraswat <128489200+charus-dev@users.noreply.github.com>
2024-08-22 14:44:38 +0530

    Support File

---
+++ src/app/support/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+const Privacy = () => {
+    return (
+        <>
+        <div className="main-privacy-content px-32 py-16">
+            <div className="content-container">
+                <div className="sidebar">
+                    <h1 className="privacy-policy-heading">Support</h1>
+                </div>
+                <div className="main-content">
+                    <h2 className="Introduction">We're Here to Help You Succeed</h2>
+                    <br></br>
+                    <p className="rules">
+                    At AI Minds, we believe that learning should be an empowering experience, and we're committed to supporting you every step of the way. 
+                    </p>
+                    <p className="rules">
+                    You can contact us via email at xyz@example.com. Our support team is available [mention your support hours, e.g., 24/7, or specific hours], and we aim to respond to all inquiries as quickly as 
+                    possible, typically within [mention your response time, e.g., 24 hours].
+                    </p>
+                    <h3>How to Get in Touch</h3>
+                    <p className="rules">
+                    If you encounter any issues or have any queries, please don't hesitate to reach out to our support team. 
+                    </p>
+                    <p className="rules">
+                    We understand that every learner’s journey is unique, and we're dedicated to providing personalized assistance 
+                    to ensure your experience is smooth and rewarding.
+                    </p>
+                    <h3>Your Feedback Matters</h3>
+                    <p className="rules">
+                    Your feedback is vital to us. If you have any suggestions or comments, please let us know.
+                    </p>
+                    <p className="rules">
+                    Thank you for choosing AI Minds. We’re here to support you and help you reach your goals.
+                    </p>
+                </div>
+            </div>
+        </div>
+         <footer style={{ textAlign: "center", marginTop: "20px" }}>
+          <p>&copy; 2024 Rightsol. All rights reserved.</p>
+        </footer>
+        </>
+    )
+}
+
+export default Privacy
